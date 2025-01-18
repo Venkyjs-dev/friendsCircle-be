20cd8017fc78168396d0d476c55fe47b6d01ef86
Venkyjs-dev <venkatesh131755@gmail.com>
2025-01-18 18:01:45 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://venkatesh131755:yGiIuGKu7odxPDk5@mynodecluster.r553g.mongodb.net/devTinder"
+    "mongodb+srv://<UserName>:<password>@mynodecluster.r553g.mongodb.net/<db name>"
   );
 };
 
